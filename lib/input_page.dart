f907760c3583ae98cc02097cb0bf56a660c9c488
foziahussaini <fauziakhudadad@gmail.com>
2026-02-24 21:25:36 +0430

    extracting widgets

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -12,15 +12,40 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Container(
-        margin: EdgeInsets.all(16),
-        decoration: BoxDecoration(
-            color: Color(0xFF1D1E33),
-            borderRadius: BorderRadius.circular(16)),
-        height: 200,
-        width: 170,
-        child: Center(child: Text('Body Text')),
-      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(child: ReuseableCard()),
+              Expanded(child: ReuseableCard())
+            ],
+          ),
+          Expanded(child: ReuseableCard(),),
+          Row(
+            children: [
+              Expanded(child: ReuseableCard()),
+                  Expanded(child: ReuseableCard())
+            ],
+          )
+        ],
+      )
+    );
+  }
+}
+
+class ReuseableCard extends StatelessWidget {
+
+  final Color? color;
+  ReuseableCard({this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+          color: color ?? Color(0xFF1D1E33),
+          borderRadius: BorderRadius.circular(16)),
+      child: Center(child: Text('Body Text')),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: Colors.black87,
         appBarTheme: AppBarTheme(backgroundColor: Colors.black12,)
